b0410885639ef1c05f9c3bd057238f256827e0d2
akc14 <andrew.khoh@globalfoundries.com>
2022-09-23 13:54:50 +0000

    new

--- src/Calculator.jl
+++ src/Calculator.jl
@@ -1,5 +1,22 @@
 module Calculator
 
+export interest, rate
+
+"""
+ interest(amount, rate)
+
+Calculate interest from an 'amount' and interest rate of 'rate'
+
+"""
+
 # Write your package code here.
 
+function interest(amount, rate)
+  return amount * (1 + rate)
 end
+
+function rate(amount, interest)
+  interest / amount
+end
+
+end # end module
